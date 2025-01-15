40d3996f95ed9c45261cc7471f2a48af56aa13e9
ChonKb <bumasskirb@gmail.com>
2025-01-14 20:53:03 -0500

    14/01

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'; //Importamos el custom_element_schema
 
 @NgModule({
   imports: [
@@ -14,6 +14,7 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA] // Añadimos el custom_element_schema
 })
 export class HomePageModule {}
